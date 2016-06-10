08a9281532920444b226770f8c2c94083503fb7a
Angie <angelicagarciaalejo@gmail.com>
2016-06-10 16:09:50 -0500

    Vistas para actualizar y eliminar utilizando dos formularios en el contexto

--- apps/adopcion/urls.py
+++ apps/adopcion/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
 
-from apps.adopcion.views import index_adopcion, SolicitudList, SolicitudCreate
+from apps.adopcion.views import index_adopcion, SolicitudList, SolicitudCreate, SolicitudUpdate, \
+	SolicitudDelete
 
 urlpatterns = [
     url(r'^index$', index_adopcion),
     url(r'^solicitud/listar$', SolicitudList.as_view(), name='solicitud_listar'),
     url(r'^solicitud/nueva$', SolicitudCreate.as_view(), name='solicitud_crear'),
-
+    url(r'^solicitud/editar/(?P<pk>\d+)$', SolicitudUpdate.as_view(), name='solicitud_editar'),
+    url(r'^solicitud/eliminar/(?P<pk>\d+)$', SolicitudDelete.as_view(), name='solicitud_eliminar'),
 
 ]
