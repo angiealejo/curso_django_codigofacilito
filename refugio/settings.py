3e886def280ab5560b85c463a3aad4db8a0b8afd
Angie <angelicagarciaalejo@gmail.com>
2016-06-11 17:02:08 -0500

    Implementando login

--- refugio/settings.py
+++ refugio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+from django.core.urlresolvers import reverse_lazy
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -130,3 +131,6 @@ STATIC_URL = '/static/'
 
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+
+LOGIN_REDIRECT_URL = reverse_lazy('adopcion:solicitud_listar')
--- refugio/urls.py
+++ refugio/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth.views import login
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^mascota/', include('apps.mascota.urls', namespace='mascota')),
     url(r'^adopcion/', include('apps.adopcion.urls', namespace='adopcion')),
     url(r'^usuario/', include('apps.usuario.urls', namespace='usuario')),
+    url(r'^$', login, {'template_name':'index.html'}, name='login')
 
 ]
