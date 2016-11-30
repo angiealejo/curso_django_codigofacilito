c8e3585085c02e698221211d9f56c7a233b633d4
Angelica <angelicagarciaalejo@gmail.com>
2016-11-30 12:56:25 -0600

    correccion en un url password reset done

--- refugio/urls.py
+++ refugio/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
         {'template_name':'registration/password_reset_form.html',
         'email_template_name': 'registration/password_reset_email.html'}, 
         name='password_reset'), 
-    url(r'^reset/password_reset_done', password_reset_done, 
+    url(r'^password_reset_done', password_reset_done, 
         {'template_name': 'registration/password_reset_done.html'}, 
         name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$', password_reset_confirm, 
